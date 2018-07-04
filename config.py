fd26c03206a8cdfd39b34a72004252a141b417c9
Ubuntu <ubuntu@ip-172-31-4-151.ec2.internal>
2018-07-04 21:46:29 +0000

    Removed secret keys

--- config.py
+++ config.py
@@ -1,6 +1,122 @@
-import uuid
+import json
+from functools import wraps
 
-APP_NAME = "SalusMind"
+from flask import session
+from flask import redirect
+
+from sparkpost import SparkPost
+from pymongo import MongoClient
+
+
+data = None
+
+with open("~/secret.json", 'r') as json_file:
+    data = json.loads(json_file.read())
+
+APP_NAME = data['app_name']
 DEBUG = True
-THREADS_PER_PAGE = 2
-SECRET_KEY = str(uuid.uuid4().hex)
+SECRET_KEY = APP_NAME
+
+client = MongoClient()
+db = client[APP_NAME]
+
+sp = SparkPost(data['sparkpost_key'])
+
+
+class g:
+    def __init__(self):
+        self.bcrypt = None
+
+
+class Errors:
+    def __init__(self):
+        self.status = False
+
+    def custom(self, message):
+        return {'status': self.status, 'message': message}
+
+    def invalid_request(self):
+        return self.custom('Invalid request parameters.')
+
+    def credentials_invalid(self):
+        return self.custom('The username or password incorrect.')
+    
+    def confirm_email(self):
+        return self.custom('A confirmation email has been sent to '+email+'. You must verify before signing in.')    
+
+    def missing_parameters(self):
+        return self.custom('Please fill all fields.')
+    
+    def user_suspended(self):
+        return self.custom('User account is suspended. Contact support@salusmind.com for more information.')
+    
+    def user_exists(self, email):
+        return self.custom('User account with email '+email+' already exists.')
+
+
+class Success:
+    def __init__(self):
+        self.status = True
+    
+    def custom(self, message):
+        return {'status': self.status, 'message': message}
+
+    def confirmation(self):
+        return self.custom('Email verification successful.')
+
+
+
+def send_confirmation_email(fname, email, confirm_key):
+    return sp.transmissions.send(
+        use_sandbox=False,
+        recipients=[email],
+        html='''
+        <p>Hey '''+fname+''',</p>
+        <p>
+            You are recieving this email because you have signed up 
+            to <a href="https://salusmind.com">Salus Mind</a>, a mental health monitoring service.<br><br>
+            Before you can get started, you must confirm your email using the following link:<br>
+            <a href="https://salusmind.com/api/user/confirm/'''+email+'''/'''+confirm_key+'''">Confirm Email Address</a>
+         <p>
+         <p>
+            Thanks,<br>
+            The Salus Mind Team
+         </p>
+	 <p>
+            <small>
+                P.S. This email was automatically generated. Contact <a href="mailto:support@salusmind.com">support@salusmind.com</a> 
+                for any questions, comments, issues, or concerns.
+            </small>
+            <br>
+            <small>P.S.S. If you did not sign up for this service, please disregard this email.</small>
+         </p>
+
+        ''',
+        from_email='hello@salusmind.com',
+        subject='Welcome to Salus Mind'
+    )
+
+
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'user' not in session:
+            return redirect('/')
+        else:
+            user = db.users.find_one({'email': session['user']['email']})
+            user['_id'] = str(user['_id'])
+            session['user'] = user
+        return f(*args, **kwargs)
+    return decorated
+
+
+def non_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if 'user' in session:
+            return redirect('/profile')
+        return f(*args, **kwargs)
+    return decorated
+
+
--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 import datetime
 import uuid
 
-from pymongo import MongoClient
 from common import config
 
 
@@ -20,5 +19,73 @@ class Organization:
         return
 
 
-client = MongoClient()
-db = client[config.APP_NAME]
+db = config.db
+
+
+def login(email, password):
+    try:
+        user = db.users.find_one({'email': email})
+        if user and config.g.bcrypt.check_password_hash(user['password'], password):
+            if user['isConfirmed']:
+                if not user['isSuspended']:
+                    user['_id'] = str(user['_id'])
+                    return config.Success().custom(user)
+                else:
+                    return config.Errors().user_suspended()
+            else:
+                return config.Errors().confirm_email(email)
+        else:
+            return config.Errors().credentials_invalid()
+    except:
+        return config.Errors().missing_parameters()
+
+
+def signup(fname, lname, email, password):
+    print(fname, lname, email, password)
+    try:
+        exist = db.users.find_one({'email': email})
+        if not exist:
+            confirm_key = str(uuid.uuid4().hex)
+            user_id = db.users.insert_one({
+                'fname': fname,
+                'lname': lname,
+                'email': email,
+                'password': config.g.bcrypt.generate_password_hash(password),
+                'isConfirmed': False,
+                'isSuspended': False,
+                'confirmKey': confirm_key
+            })
+            config.send_confirmation_email(fname, email, confirm_key)
+            return config.Errors().confirm_email(email)
+        else:
+            return config.Errors().user_exists(email)
+    except:
+        return config.Errors().missing_parameters()
+
+
+
+def confirm(email, confirm_key):
+    try:     
+        updated = db.users.update_one({'email': email, 'confirmKey': confirm_key}, {'$set': {'isConfirmed': True}}, upsert=False)
+        if updated.matched_count > 0:
+            return config.Success().confirmation()
+        else:
+            return config.Error().confirmation()
+    except:
+        return config.Errors().missing_parameters()
+
+
+def forgot(email):
+    pass
+
+
+def connect_account(email, media, account):
+    try:
+        updated = db.users.update_one({'email': email}, {'$set': {media: account}}, upsert=False)
+        if updated.matched_count > 0:
+            return config.Success().custom(media+" has been added successfully")
+        else:
+            return config.Error().custom("Failed to add "+media+" account")
+    except:
+        return config.Errors().custom("Service unavailable")
+
